81ebd014583a8f31578dccd24d37f19eac860b3f
hodard <hodardhazwinayo@gmail.com>
2018-07-08 13:05:04 +0200

    Fire with php some change onpassword privelegies

--- admin/dbconnect.php
+++ admin/dbconnect.php
@@ -1,4 +1,4 @@
 <?php
 //connect to mysql database
-$con = mysqli_connect("localhost", "root", "lecteur1994ange", "hodard") or die("Error " . mysqli_error($con));
+$con = mysqli_connect("localhost", "root", " ", "hodard") or die("Error " . mysqli_error($con));
 ?>
--- hodard.sql
+++ hodard.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 03, 2018 at 04:20 PM
+-- Generation Time: Jul 08, 2018 at 11:04 AM
 -- Server version: 5.7.22-0ubuntu0.16.04.1
 -- PHP Version: 7.0.30-0ubuntu0.16.04.1
 
